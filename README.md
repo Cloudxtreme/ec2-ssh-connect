a6782cab6f54ad7130d208a90f1b3f4ca005f0ca
Sean Dugan <sean.dugan@c2fo.com>
2014-06-23 15:32:07 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 ec2-ssh-connect
 ===============
 
-Always up to date bash script for SSHing into EC2 servers
+Self updating bash script for showing menu list of current EC2 servers, and then allow you to choose which server to SSH into using its private IP address. It will store a cache file with the info from AWS it needs to display the list of servers. If the cache file is older than a day it will force a refresh.
+
+Unfortunately there are lots of variables that you will need to specificy in order to keep this script flexible between environments. I hope to make it simpler in the future.
+
+## Setup
+
+1. Install Amazon EC2 CLI Tools and Java JDK if not already installed. http://docs.aws.amazon.com/AWSEC2/latest/CommandLineReference/set-up-ec2-cli-linux.html#setting_up_ec2_command_linux
+2. Grant IAM user the EC2 DescribeInstances role if not an IAM admin user. (I think this is the only one needed but I haven't verified myself yet.)
+3. Download the scripts.
+4. Update variables for your particular environment. EC2_HOME, JAVA_HOME, AWS_ACCESS_KEY, AWS_SECRET_KEY, LIST_FILE, userlogin, and region
+5. Make the file executable.
+6. Try it out! When the list of servers is displayed just type in the number of the server desired then hit Enter.
---
+++ ec2-ssh-connect.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Must have the Amazon EC2 CLI Tools and Java JDK installed. Update EC2_HOME and JAVA_HOME as appropriate.
+# http://docs.aws.amazon.com/AWSEC2/latest/CommandLineReference/set-up-ec2-cli-linux.html#setting_up_ec2_command_linux
+
+export EC2_HOME='/usr/local/ec2/api'
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home'
+export PATH=$PATH:$EC2_HOME/bin
+export AWS_ACCESS_KEY='<Input IAM AWS Access Key>'
+export AWS_SECRET_KEY='<Input IAM AWS Secret Key>'
+
+# The userlogin variable is used for the path of the
+userlogin="<your username>"
+region="<desired region>"
+
+# Update path as appropriate or wherever you desire.
+LIST_FILE="/Users/$userlogin/.servers_cache.txt"
+
+# If the server list cache doesn't exist, create it. If older than a day, update it. Otherwise continue.
+
+function updateList(){
+  ec2-describe-instances --region $region > $LIST_FILE
+}
+
+if [[ ! -f "$LIST_FILE" ]]; then
+  echo "No server list cache file at $LIST_FILE, updating..."
+  updateList
+  echo "...done!"
+elif [[ "$(find "$LIST_FILE" -mtime +1)" == "$LIST_FILE" ]]; then
+  echo "Server list cache file is older than 1 day, updating..."
+  updateList
+  echo "...done!"
+else
+  echo "Server list cache file is recent."
+fi
+
+# Get the list of private IPs to use to match up with other data.
+IP_LIST=$(grep "PRIVATEIPADDRESS" $LIST_FILE | awk '{print $2}')
+
+n=0
+instance_array=()
+for ip in $(echo $IP_LIST); do
+  instance=$(grep -w $ip $LIST_FILE | grep "INSTANCE" | awk '{print $2}')
+  tagname=$(grep $instance $LIST_FILE | grep "TAG" | grep "Name" | awk '{print $5 $6 $7 $8 $9}')
+  instance_array[$n]="$tagname $ip"
+  ((++n))
+done
+
+# http://stackoverflow.com/a/11789688
+IFS=$'\n' sorted_array=($(sort <<<"${instance_array[*]}"))
+
+echo ============================
+echo "     EC2 Server List      "
+echo ============================
+echo ""
+
+eval set ${sorted_array[@]}
+select opt in "${sorted_array[@]}"
+do
+  choice=$(echo $opt | awk '{print $2}')
+  ssh -p 22 $userlogin@$choice
+exit
+done
